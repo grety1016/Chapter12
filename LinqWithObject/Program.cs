63321e44852a8794766baa7f0d595facff86f468
grety1016 <snl1016@163.com>
2020-07-09 14:58:32 +0800

    1

---
+++ LinqWithObject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace LinqWithObject
+{
+    class Program
+    {
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+        static void LinqWithArrayOfString()
+        {
+            var names = new string[]{"Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+            var query = names.Where(name => name.Length > 4);
+                foreach (string item in query)
+                {
+                    WriteLine(item) ;
+                }
+        }
+        static void Main(string[] args)
+        {
+             LinqWithArrayOfString();
+        }
+    }
+}
